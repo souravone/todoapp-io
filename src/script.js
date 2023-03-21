0256481b0bfcf5d749f4d9350605f1a1bc635dcb
sourav <souravdas360@gmail.com>
2023-03-21 11:01:39 +0530

    implemented desktop layout

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log("hi!");
+const form = document.querySelector(".form__input");
+const submitButton = document.querySelector(".form__input__button");
+const formInput = document.querySelector(".form__input__value");
+const todoItems = document.querySelector(".todo__items");
+const todoItemCheckbox = document.querySelector(".todo__item__checkbox");
+const todoItemText = document.querySelector(".todo__item__text");
+const todoItemCross = document.querySelector(".todo__item__cross");
+const totalItems = document.querySelector(".todo__below__left");
+const itemPlural = document.querySelector(".only-s");
+const allTasks = document.querySelector(".todo__below__list__item--all");
+const activeTasks = document.querySelector(".todo__below__list__item--active");
+const completedTasks = document.querySelector(
+  ".todo__below__list__item----completed"
+);
+
+console.log(form);
